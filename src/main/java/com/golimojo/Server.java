74576249bcfe51ca6432ffb6959e1daecc0d125c
Curtis Bartley <curtisb@almostinfinite.com>
2015-05-06 15:41:39 -0700

    Added 'Next' links to tabs/pages on the website, added robots.txt, and some miscellaneous cleanup. (most source changes from 2005)

--- src/main/java/com/golimojo/Server.java
+++ src/main/java/com/golimojo/Server.java
@@ -75,6 +75,7 @@ public class Server extends ShellTools
         String pathToXpi = FirefoxExtensionConfigurationFactory.lazyCreateXpi("localhost", 8085);
 //      system("cmd /c start /wait Firefox -Profile test-files/FirefoxTestProfile -install-global-extension {pathToXpi}", extraEnvVariables, sub("pathToXpi", pathToXpi));
 //      system("cmd /c start /wait Firefox -Profile test-files/FirefoxTestProfile http://www.washingtonmonthly.com/ http://localhost:8085/javascript/tablog.html", extraEnvVariables);
+//      system("cmd /c start /wait Firefox -Profile test-files/FirefoxTestProfile http://localhost:8085/index.html", extraEnvVariables);
         system("/Applications/Firefox.app/Contents/MacOS/firefox -CreateProfile Golimojo");
         system("/Applications/Firefox.app/Contents/MacOS/firefox --jsconsole -P Golimojo {pathToXpi} http://www.jsc.nasa.gov/Bios/htmlbios/aldrin-b.html", sub("pathToXpi", pathToXpi));
     }
